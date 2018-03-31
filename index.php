505ccde748241f296ff18d9a8ea9c9559b8f2aaf
Quocdv <quoc.doan@propzy.com>
2018-04-01 01:01:57 +0700

    sas

--- index.php
+++ index.php
@@ -42,14 +42,13 @@ $registry->set('objects', $objects);
 
 // Database
 if(DB_DRIVER == 'mysqli') {
-    $test_connect = new DB("mysqli", "35.224.9.146", "rent_rooms", "ada$@#sf323", "rent_rooms");
-    var_dump($test_connect);
-    die();
-    //$db = new DB("mysqli", "124.158.14.30", "propzydeveloper", "DevPropzy2017!@#", "propzy_vietnam");
-    //$query = $db->query("select Title,Photo,Address,CityID,DistrictID,Latitude,Longitude,ListingTypeName from listing where CityID = 1 limit 0,10000");
-    $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+    $db = new DB("mysqli", "124.158.14.30", "propzydeveloper", "DevPropzy2017!@#", "propzy_vietnam");
+    $query = $db->query("select Title,Photo,Address,CityID,DistrictID,Latitude,Longitude,ListingTypeName from listing where CityID = 1 limit 0,10000");
+    var_dump($query->rows);
     var_dump($db);
     die();
+    $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     $registry->set('db', $db);
 }
 if(DB_DRIVER == 'mongodb') {
