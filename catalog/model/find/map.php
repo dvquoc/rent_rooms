306b198ff3a3192636731b3e381bd23b4b379c20
Quocdv <quoc.doan@propzy.com>
2018-04-04 15:55:42 +0700

    add find map

---
+++ catalog/model/find/map.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2/1/2018
+ * Time: 9:51 AM
+ */
+class ModelFindMap extends Model {
+    public function common(){
+        $result = $this->db->ladingpage->find();
+        if(count($result) > 0)
+            return $result->toArray();
+        return false;
+    }
+    public function regionListing($data_region){
+        $data = array(
+                    'type' =>'Polygon',
+                    'coordinates'=>array($data_region)
+                );
+        $result = $this->db->listing->find(array(
+            'location'=>['$geoWithin'=>['$geometry'=>$data]],
+        ),
+        array(
+            'limit'=>12
+        )
+        );
+        if(count($result) > 0)
+            return $result->toArray();
+        return false;
+    }
+}
